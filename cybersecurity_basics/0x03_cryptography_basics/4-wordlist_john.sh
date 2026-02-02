54153bde4adc6661ddaa1f9dc04f2aeb0ad4afac
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2026-02-02 04:37:32 -0500

    standart for commit

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-john $1 -w /usr/share/wordlists/rockyou.txt 
-john --show "$1" | cut -d: -f2 | head -n -1 > 4-password.txt
+#!/bin/bash
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null && john --show "$1" | grep ":" | cut -d: -f2 > 4-password.txt
