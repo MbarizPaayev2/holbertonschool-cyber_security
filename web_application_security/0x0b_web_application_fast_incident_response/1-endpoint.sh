b29a5b30f8b28cd7526deaa1d015f59583cc2151
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2026-04-11 09:40:47 -0400

    standart for commit

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $7}' "$1" | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}'
+awk -F'"' '{print $2}' logs.txt | awk '{print $2}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
