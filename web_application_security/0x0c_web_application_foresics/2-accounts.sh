701c2bd0793ccccf549accb5f411349c4784b96b
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2026-04-13 08:42:30 -0400

    standart for commit

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Accepted" | awk '{print $9}' | uniq
+tail -n 1000 $1 | grep "Failed" | awk '{print $9}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
