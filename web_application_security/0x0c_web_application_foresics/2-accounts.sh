6a0dc18aa7d716e3405badb3770da7bc77ee2d05
MbarizPaayev2 <pashayevmubariz11@gmail.com>
2026-04-13 08:45:16 -0400

    standart for commit

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 $1 | grep "Failed" | awk '{print $9}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+tail -n 1000 auth.log | grep -E "root" | sort | uniq -c | sort -nr | head -n 1|awk '{print $12}'
