9ea86bd4efbc0ba8a5bb56d767844fe3f9e0234f
MbarizPaayev2 <Pashayevmubariz11@gmail.com>
2026-05-24 18:56:15 +0400

    hbtn task solve

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -14,5 +14,26 @@ def post_request(url, body_params = {})
 
   puts "Response status: #{response.code} #{response.message}"
   puts 'Response body:'
-  puts JSON.pretty_generate(JSON.parse(response.body))
+
+  parsed = JSON.parse(response.body)
+  puts parsed.empty? ? '{}' : JSON.pretty_generate(parsed)
+endrequire 'net/http'
+require 'json'
+
+def post_request(url, body_params = {})
+  uri = URI(url)
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == 'https')
+
+  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  request.body = JSON.generate(body_params)
+
+  response = http.request(request)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts 'Response body:'
+
+  parsed = JSON.parse(response.body)
+  puts parsed.empty? ? '{}' : JSON.pretty_generate(parsed)
 end
