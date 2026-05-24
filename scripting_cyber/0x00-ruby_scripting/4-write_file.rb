fe351617ea012209cbfc969004a99d5b5c1de347
MbarizPaayev2 <Pashayevmubariz11@gmail.com>
2026-05-24 18:43:43 +0400

    hbtn task solve

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,11 +1,11 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
-  file1_data = JSON.parse(File.read(file1_path))
+  data1 = JSON.parse(File.read(file1_path))
+  data2 = JSON.parse(File.read(file2_path))
 
-  file2_data = JSON.parse(File.read(file2_path))
+  merged = data2 + data1
 
-  merged_data = file2_data + file1_data
-
-  File.write(file2_path, JSON.pretty_generate(merged_data))
+  File.write(file2_path, JSON.generate(merged))
+  puts "Merged JSON written to #{file2_path}"
 end
