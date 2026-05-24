613a717676ce4eb29b2b5308db242430ba11d2df
MbarizPaayev2 <Pashayevmubariz11@gmail.com>
2026-05-24 18:57:29 +0400

    hbtn task solve

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,39 +1,19 @@
+#!/usr/bin/env ruby
 require 'net/http'
 require 'json'
+require 'uri'
 
 def post_request(url, body_params = {})
-  uri = URI(url)
-
+  uri = URI.parse(url)
   http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = (uri.scheme == 'https')
+  http.use_ssl = (uri.scheme == "https")
 
-  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-  request.body = JSON.generate(body_params)
+  request = Net::HTTP::Post.new(uri.request_uri, { 'Content-Type' => 'application/json' })
+  request.body = body_params.to_json
 
   response = http.request(request)
 
   puts "Response status: #{response.code} #{response.message}"
-  puts 'Response body:'
-
-  parsed = JSON.parse(response.body)
-  puts parsed.empty? ? '{}' : JSON.pretty_generate(parsed)
-endrequire 'net/http'
-require 'json'
-
-def post_request(url, body_params = {})
-  uri = URI(url)
-
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = (uri.scheme == 'https')
-
-  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-  request.body = JSON.generate(body_params)
-
-  response = http.request(request)
-
-  puts "Response status: #{response.code} #{response.message}"
-  puts 'Response body:'
-
-  parsed = JSON.parse(response.body)
-  puts parsed.empty? ? '{}' : JSON.pretty_generate(parsed)
+  puts "Response body:"
+  puts response.body
 end
